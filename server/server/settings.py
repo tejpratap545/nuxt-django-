f0af21c2dc182433444b4137e2c17bcc5b90f9e3
Tejpratap545 <tejpratapsingh545@gmail.com>
2020-05-25 01:46:27 +0530

    creating custom user model

--- server/server/settings.py
+++ server/server/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app.Users',
+    
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,13 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db4',
+        'USER': 'postgres',
+        'PASSWORD': 'DjangoPostgresql',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
@@ -118,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL = 'Users.User'
